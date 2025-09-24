3232dfc582336407a37158a203232de856b43e0e
Deepti <deepti.narayan@sunplussoftware.com>
2025-09-24 17:21:41 +0530

    Changes done in ProtectedRoute

--- src/Components/ProtectedRoute.js
+++ src/Components/ProtectedRoute.js
@@ -1,15 +1,28 @@
+// import { Navigate } from "react-router-dom";
+
+// const ProtectedRoute = ({ children }) => {
+//   const token = localStorage.getItem("token");
+
+//   if (!token) {
+//     return <Navigate to="/" />;  // Redirect to login if no token
+//   }
+
+//   return children;
+// };
+
+// export default ProtectedRoute;
+
+
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
   const token = localStorage.getItem("token");
 
   if (!token) {
-    return <Navigate to="/" />;  // Redirect to login if no token
+    return <Navigate to="/login" replace />; // Redirect correctly to login
   }
 
   return children;
 };
 
 export default ProtectedRoute;
-
-
